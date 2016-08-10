1158be1a690adf535020a307069bd77f6eb1aef7
Rik <mr.valery85@yandex.ru>
2016-08-10 19:09:19 +0300

    Created UserProfile

--- src/main/java/accounts/UserProfile.java
+++ src/main/java/accounts/UserProfile.java
@@ -1,6 +1,25 @@
 package accounts;
 
 public class UserProfile {
+    private String login;
+    private String pass;
+    private String email;
 
+    public UserProfile(String login, String pass, String email){
+        this.login = login;
+        this.pass = pass;
+        this.email = email;
+    }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
