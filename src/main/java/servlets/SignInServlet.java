3cf073614de0066baeb5df14d6c6d23410b366ad
Rik <mr.valery85@yandex.ru>
2016-08-11 15:46:17 +0300

    Base done

--- src/main/java/servlets/SignInServlet.java
+++ src/main/java/servlets/SignInServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import accounts.AccountService;
+import accounts.UserProfile;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,17 +20,31 @@ public class SignInServlet extends HttpServlet {
                         HttpServletResponse response) throws IOException {
 
        String login = request.getParameter("login");
-       String password = request.getParameter("password");
-        response.getWriter().println("Authorized:" + login );
-//        if (accountService.getUserByLogin(login).getLogin().equals(login)  ){
-//            response.setContentType("text/html;charset=utf-8");
-//            response.getWriter().println("Authorized:" + login);
+        String password = request.getParameter("password");
+
+        UserProfile profile = accountService.getUserByLogin(login);
+
+        if ((profile == null) || (!password.equals(profile.getPass())) ){
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println("Unauthorized");
+            return;
+
         }
 
-
-
-
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("Authorized: " + login);
+        response.setStatus(HttpServletResponse.SC_OK);
     }
+}
 
+
+//        if (accountService.getUserByLogin(login).getLogin().equals(login)  &&
+//                (accountService.getUserByLogin(login).getPass().equals(password)) ){
+//            response.getWriter().println("Authorized:" + login);
+//        }
+//        else {
+//            response.getWriter().println("Unauthorized:");
+//        }
 
 //}
